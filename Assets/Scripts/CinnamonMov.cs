8ef052d2154b27b45e340274fe8b51a4e307ff97
Sofivd <svd08062001svd@gmail.com>
2024-04-22 17:00:30 +0100

    Animacion Cinnamon caminar

--- Assets/Scripts/CinnamonMov.cs
+++ Assets/Scripts/CinnamonMov.cs
@@ -1,36 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEditorInternal;
 using UnityEngine;
+
 
 public class CinnamonMov : MonoBehaviour
 {
     public float movX, movZ;
     Rigidbody rb;
-    float speed = 8f;
+    public float speed = 8f;
     public bool sobreSuelo = false;
     public bool saltar = false;
     public float fuerzadeSalto = 10f;
     public GameObject Slime2;
     public GameObject Transformacion;
     public GameObject CinnamonBase;
+    private CharacterController characterController;
+    private Animator animatorController;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        animatorController = GetComponent<Animator>();
         Slime2.gameObject.SetActive(false);
         Transformacion.gameObject.SetActive(false);
         CinnamonBase.gameObject.SetActive(true);
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         movX = Input.GetAxis("Horizontal");
         movZ = Input.GetAxis("Vertical");
+
         Vector3 nuevaVelocidad = new Vector3(movX * speed, rb.velocity.y, movZ * speed);
         rb.velocity = nuevaVelocidad;
+
+        // TOD: MIRAR LA SUMA DE LOS ANGULOS EN GRADOS PARA QUE NO PASE DE 90.
+        Debug.Log(transform.rotation.ToEulerAngles().y);
+        if(transform.rotation.eulerAngles.y < 90 || transform.rotation.eulerAngles.y > -90)
+        {
+            transform.Rotate(new Vector3(0,movX * 90,0) * Time.deltaTime);
+        }
+       
+        //characterController.Move(nuevaVelocidad * speed * Time.deltaTime);
+
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -45,6 +61,8 @@ public class CinnamonMov : MonoBehaviour
             sobreSuelo = false;
         }
 
+        animatorController.SetFloat("Velocidad",movZ );
+
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -58,7 +76,6 @@ public class CinnamonMov : MonoBehaviour
 
         }
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Estrella")
@@ -66,6 +83,7 @@ public class CinnamonMov : MonoBehaviour
             Slime2.gameObject.SetActive(true);
             Transformacion.gameObject.SetActive(true);
             CinnamonBase.gameObject.SetActive(false);
+            Destroy(other.gameObject);
         }
     }
 }
