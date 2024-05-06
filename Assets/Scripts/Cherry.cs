6b4152734f36745e42eae26adc00e16783ac58b1
Sofivd <svd08062001svd@gmail.com>
2024-05-06 17:47:55 +0100

    Script cherry

---
+++ Assets/Scripts/Cherry.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cherry : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Transform cinna;
+    public CinnamonMov cinnamon;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if( tieneLlave = true)
+        {
+          //  float step = speed * Time.deltaTime;
+         //   transform.position = Vector3.MoveTowards(transform.position, cinna.position, step);
+        }
+        
+    }
+}
